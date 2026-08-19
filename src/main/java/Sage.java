a2d0ec865a50459277eb8923ced5c0a0d669b5d8
Sia Pathania <siapathania22@gmail.com>
2026-08-19 17:08:28 +0800

    Personalize Sage greeting and farewell

--- src/main/java/Sage.java
+++ src/main/java/Sage.java
@@ -3,12 +3,22 @@
  */
 public class Sage {
     public static void main(String[] args) {
+        String divider = "____________________________________________________________";
         String banner = " ____                   \n"
                 + "/ ___|  __ _  __ _  ___ \n"
                 + "\\___ \\ / _` |/ _` |/ _ \\\n"
                 + " ___) | (_| | (_| |  __/\n"
                 + "|____/ \\__,_|\\__, |\\___|\n"
                 + "             |___/      \n";
-        System.out.println(banner);
+
+        System.out.println(divider);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Sage.");
+        System.out.println("I'm here whenever you feel like chatting!");
+        System.out.println(divider);
+        System.out.println("What would you like to talk about?");
+        System.out.println(divider);
+        System.out.println("Bye for now! Take care, and see you soon!");
+        System.out.println(divider);
     }
 }
