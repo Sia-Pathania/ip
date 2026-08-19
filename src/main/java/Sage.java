2455b36895a0ea24a3d2ff4b1d4b011212b215fa
Sia Pathania <siapathania22@gmail.com>
2026-08-19 16:59:29 +0800

    Rename Duke chatbot to Sage

---
+++ src/main/java/Sage.java
@@ -0,0 +1,14 @@
+/**
+ * Starts the Sage chatbot application.
+ */
+public class Sage {
+    public static void main(String[] args) {
+        String banner = " ____                   \n"
+                + "/ ___|  __ _  __ _  ___ \n"
+                + "\\___ \\ / _` |/ _` |/ _ \\\n"
+                + " ___) | (_| | (_| |  __/\n"
+                + "|____/ \\__,_|\\__, |\\___|\n"
+                + "             |___/      \n";
+        System.out.println(banner);
+    }
+}
