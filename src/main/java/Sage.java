0f40ccbba2ec6b394c4fd568fe49231b3e05d874
Sia Pathania <siapathania22@gmail.com>
2026-08-20 06:34:53 +0800

    Represent tasks with a dedicated Task class

--- src/main/java/Sage.java
+++ src/main/java/Sage.java
@@ -28,7 +28,7 @@ public class Sage {
         System.out.println("What can I do for you?");
         System.out.println(divider);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         try (Scanner scanner = new Scanner(System.in)) {
@@ -48,17 +48,17 @@ public class Sage {
                 } else if (command.startsWith("mark ")) {
                     int taskNumber = Integer.parseInt(command.substring(5));
                     int taskIndex = taskNumber - 1;
-                    tasks[taskIndex] = "[X] " + tasks[taskIndex].substring(4);
+                    tasks[taskIndex].markAsDone();
                     System.out.println("Nice! I've marked this task as done:");
                     System.out.println("  " + tasks[taskIndex]);
                 } else if (command.startsWith("unmark ")) {
                     int taskNumber = Integer.parseInt(command.substring(7));
                     int taskIndex = taskNumber - 1;
-                    tasks[taskIndex] = "[ ] " + tasks[taskIndex].substring(4);
+                    tasks[taskIndex].markAsNotDone();
                     System.out.println("OK, I've marked this task as not done yet:");
                     System.out.println("  " + tasks[taskIndex]);
                 } else {
-                    tasks[taskCount] = "[ ] " + command;
+                    tasks[taskCount] = new Task(command);
                     taskCount++;
                     System.out.println("added: " + command);
                 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,52 @@
+/**
+ * Represents a task with a description and completion status.
+ */
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    /**
+     * Creates a task that is initially not done.
+     *
+     * @param description the task description
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Marks this task as done.
+     */
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    /**
+     * Marks this task as not done.
+     */
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+    /**
+     * Returns the icon representing this task's completion status.
+     *
+     * @return {@code "X"} when done, or a space when not done
+     */
+    public String getStatusIcon() {
+        return isDone
+                ? "X"
+                : " ";
+    }
+
+    /**
+     * Returns this task in the format displayed in Sage's task list.
+     *
+     * @return the status icon followed by the description
+     */
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
