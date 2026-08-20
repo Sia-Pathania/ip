f259503f04888a1594f92f8cc503b8474e2ac28b
Sia Pathania <siapathania22@gmail.com>
2026-08-20 12:30:48 +0800

    Add constructor and update method for deadline

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -3,6 +3,8 @@
  */
 public class Task {
     private final String description;
+    private final String type;
+    private final String by;
     private boolean isDone;
 
     /**
@@ -12,6 +14,21 @@ public class Task {
      */
     public Task(String description) {
         this.description = description;
+        this.type = "T";
+        this.by = null;
+        this.isDone = false;
+    }
+
+    /**
+     * Creates a deadline task that is initially not done.
+     *
+     * @param description the task description
+     * @param by the deadline text supplied by the user
+     */
+    public Task(String description, String by) {
+        this.description = description;
+        this.type = "D";
+        this.by = by;
         this.isDone = false;
     }
 
@@ -43,10 +60,11 @@ public class Task {
     /**
      * Returns this task in the format displayed in Sage's task list.
      *
-     * @return this todo's type and status icons followed by its description
+     * @return this task's type and status icons followed by its details
      */
     @Override
     public String toString() {
-        return "[T][" + getStatusIcon() + "] " + description;
+        String task = "[" + type + "][" + getStatusIcon() + "] " + description;
+        return by == null ? task : task + " (by: " + by + ")";
     }
 }
